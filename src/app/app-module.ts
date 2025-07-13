4f18618095fb611207ba8b0d11d56e79c9325e51
Boko <Boko@DESKTOP-CCQH80O>
2025-07-13 14:21:44 +0100

    implementing firebase connectivity

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 
+// Firebase imports
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [
     App
@@ -13,7 +18,9 @@ import { App } from './app';
     AppRoutingModule
   ],
   providers: [
-    provideBrowserGlobalErrorListeners()
+    provideBrowserGlobalErrorListeners(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ],
   bootstrap: [App]
 })
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAJ2GJf2Bma0GyIRyUK7TM429w6HRrm7Nw',
+    authDomain: 'usermanagertask.firebaseapp.com',
+    projectId: 'usermanagertask',
+    storageBucket: 'usermanagertask.firebasestorage.app',
+    messagingSenderId: '343424197582',
+    appId: '1:343424197582:web:38339cf889adc2ed90419f',
+  },
+};
